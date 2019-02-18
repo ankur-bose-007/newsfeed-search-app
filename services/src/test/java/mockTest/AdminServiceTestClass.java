48a8dde38a64ba601ca8c076aeb2e76fd88ee059
Ankur Bose <bose.ankur007@gmail.com>
2019-02-18 23:41:42 +0530

    junit added

--- services/src/test/java/mockTest/AdminServiceTestClass.java
+++ services/src/test/java/mockTest/AdminServiceTestClass.java
@@ -1,11 +1,22 @@
 package mockTest;
 
+import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
+import com.mocktest.core.entity.User;
+import com.mocktest.core.repo.UserRepository;
 import com.mocktest.core.service.AdminService;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
 
 public class AdminServiceTestClass {
 	@Rule
@@ -13,5 +24,29 @@ public class AdminServiceTestClass {
 	@InjectMocks
 	private AdminService adminService;
 	
+	
+	User user;
+	
+	
+	
+	@Mock
+	private UserRepository userRepository;
+	@Before
+	public void initialize() {
+		user=new User();
+		user.setEmailId("bose.ankur007@gmail.com");
+		user.setId(1);
+		user.setPassword("Password@10");
+		user.setRoles(new ArrayList());
+		user.setAdmin(true);
+		user.setSearchList(new ArrayList());
+		user.setUserName("Ankur Bose");
+	}
+	
+	@Test
+	public void successfulUsernameTest() {
+		when(userRepository.findByEmailIdContainingIgnoreCaseAndAdmin(user.getEmailId(), false)).thenReturn(new ArrayList<User>());
+		Assert.assertEquals(new ArrayList<User>(), adminService.getUserByUsername(user.getEmailId()));
+	}
 	
 }
